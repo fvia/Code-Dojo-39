b4e341499ca6902d5f606a6a7ebc961cea9cd604
Francesc Via <f@gaxan.com>
2016-10-17 19:35:21 +0100

    1st test pass

---
+++ Code-Dojo-39/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Dojo_39
+{
+    public class Song
+    {
+        string _song;
+        public Song(string Song) {
+            _song = Song;
+        }
+        public string Sing( string animal) { return _song.Replace("[animal1]", animal); }
+    }
+}
---
+++ Code-Dojo-39Tests/SongTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code_Dojo_39;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Dojo_39.Tests
+{
+    [TestClass()]
+    public class SongTests
+    {
+        [TestMethod()]
+        public void SingTest()
+        {
+            var song = new Song("Hello [animal1]");
+            Assert.AreEqual( song.Sing("Fox") , "Hello Fox" );
+        }
+    }
+}
